b3eea36579e13bda85ffe91d3183480955abb913
Daniel López González <230110443@itsoeh.edu.mx>
2024-12-10 22:39:14 -0600

    Daniel Lopez Gonzalez
    Se agrego en la carpeta de Stacks eljuego de las torres de Hanoi

--- Stacks/Node.py
+++ Stacks/Node.py
@@ -1,8 +1,7 @@
 class Node:
-    def __init__(self, value, next_node=None, prev_node=None):
+    def __init__(self, value, next_node=None):  
         self.value = value
         self.next_node = next_node
-        self.prev_node = prev_node 
         
     def get_value(self):
         return self.value 
@@ -13,13 +12,4 @@ class Node:
     def set_next_node(self, next_node=None):
         if not isinstance(next_node, (Node, dict, type(None))):
             raise TypeError("next_node must be of type Node, dict, or None")
-        self.next_node = next_node  
-        
-    def get_prev_node(self):
-       return self.prev_node
-          
-    def set_prev_node(self, prev_node=None):
-             if not isinstance(prev_node, (Node, dict, type(None))):
-                 raise TypeError("prev_node must be of type Node or None")
-             self.prev_node = prev_node 
-            
+        self.next_node = next_node
